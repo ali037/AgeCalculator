a39e92622d31ade5e68dbf04f38d5ada3d067c17
ali037 <38304522+ali037@users.noreply.github.com>
2020-07-05 22:50:26 -0400

    Add files via upload

---
+++ agecalculator.js
@@ -0,0 +1,5 @@
+function lifeInWeeks(age) {
+    var ageLeft= 90-age;
+    console.log("You have "+(ageLeft*365)+" days, "+ (ageLeft*52)+" months, and "+(ageLeft*12)+" months left.");
+}
+lifeInWeeks(10);
